e209a28047111a822e688617796c573a036b4013
Refik Fatih Ustok <fatih.ustok@gmail.com>
2016-09-11 10:35:34 +1200

    Initial Commit

---
+++ tradeMeTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tradeMeTask
+//
+//  Created by Refik Fatih Ustok on 11/09/16.
+//  Copyright © 2016 Refik Fatih Ustok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
